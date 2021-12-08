28e839612a0483d538f794a4fed2d1f10e14c257
Peter@workingcopy <pnothaft@gmail.com>
2021-12-08 20:01:03 -0300

    Working

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+
+function compute() {
+    var principal = document.getElementById("principal");
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+	var interest = principal.value * years * rate / 100;
+	//Check if principal greater the 0
+	if(principal.value<=0){
+		alert("Enter a positive number");
+		principal.focus();
+		return false;
+	}
+	var year = new Date().getFullYear()+parseInt(years);
+	document.getElementById("result").innerHTML= `
+	If you deposit <mark> ${principal.value} </mark>,<br>
+	at an interest rate of <mark>${rate}%</mark>.<br>
+	You will receive an amount of <mark>${interest}</mark>,<br>
+	in the year <mark>${year}</mark>
+	`;
     
 }
-        
+
+function updateRate() {
+	var rateval = document.getElementById("rate").value;
+	document.getElementById("rate_val").innerText=rateval;
+}
